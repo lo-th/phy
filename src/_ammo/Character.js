11059a85e2755e14eb7bd2cb50132ba716263fb1
lo-th <3dflashlo@gmail.com>
2023-02-14 17:49:42 +0100

    up

--- src/_ammo/Character.js
+++ src/_ammo/Character.js
@@ -1,8 +1,17 @@
 import { Body } from './Body.js';
+import { Num } from '../core/Config.js';
 
 export class Character extends Body {
 	constructor () {
 		super()
-		this.type = 'character'
+		//this.type = 'character'
+		this.num = Num[ 'character' ]
+	}
+
+	add ( o = {} ) {
+
+		o.type = 'capsule'
+		super.add( o )
+
 	}
 }
