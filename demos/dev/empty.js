a1a440e5c7e736d9b59a279e966eb85135e8da65
lo-th <3dflashlo@gmail.com>
2025-06-25 16:43:21 +0200

    physx fixed !

--- demos/dev/empty.js
+++ demos/dev/empty.js
@@ -12,12 +12,17 @@ function demo() {
 
     phy.set({forceSubstep:10})
 
-    phy.add({ type:'box', size:[10,1,10], pos:[0,-0.5,0], density:0, visible:false });
+    phy.add({ type:'box', size:[10,1,10], pos:[0,-0.5,0], visible:false });
+    phy.add({ type:'plane', size:[300,1,300], visible:true })
 
-    phy.add({ name:'a', type:'box', size:[1,3,2], pos:[2,1.5,0], density:1 });
-    phy.add({ name:'b', type:'box', size:[1,3,2], pos:[-2,1.5,0], density:1 });
+    phy.add({ type:'box', size:[1,3,2], pos:[2,1.5,0], mass:1 });
+    phy.add({ type:'box', size:[1,3,2], pos:[-2,1.5,0], density:1 });
 
+    phy.add({ type:'box', size:[1,3,2], pos:[2,4.5,0], density:1 });
+    phy.add({ type:'box', size:[1,3,2], pos:[-2,4.5,0],  });
 
+    phy.add({ type:'box', size:[1,3,2], pos:[2,7.5,0], density:1 });
+    phy.add({ type:'box', size:[1,3,2], pos:[-2,7.5,0], mass:1 });
     //phy.add({ name:'Ub', type:'sphere', size:[4], pos:[0,100,0], density:1 });
 
     //phy.add({ name:'jointTest', type:'generic', b1:'a', b2:'b', pos1:[1,0,0], pos2:[-1,0,0], limit:[['x', 0, 2], ['rx', -180, 180 ] ], collision:true, visible:true });
